4237e3941ce5ce596572674c71d906ea6a55ab91
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2023-08-06 12:13:17 +0300

    render zones headings

--- src/components/flowing-assets/flowing-assets-desktop/flowing-assets-desktop.test.tsx
+++ src/components/flowing-assets/flowing-assets-desktop/flowing-assets-desktop.test.tsx
@@ -1,13 +1,24 @@
 import { describe, it, expect } from "vitest";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import FlowingAssets_Desktop from "./flowing-assets-desktop";
-import { Assets } from "../flowing-assets-types";
+import { Zones } from "../flowing-assets-types";
 
-const mockData: Assets = [];
+const mockData: Zones = [
+  { id: "1", name: "test1" },
+  { id: "2", name: "test2" },
+  { id: "3", name: "test3" },
+];
 
 describe("FlowingAssets_Desktop", () => {
   it("should render all asset zones with a name", () => {
-    render(<FlowingAssets_Desktop assets={mockData} />);
-    expect(true).toBeTruthy();
+    render(<FlowingAssets_Desktop zones={mockData} />);
+
+    const heading1 = screen.getByRole("heading", { name: "test1" });
+    const heading2 = screen.getByRole("heading", { name: "test2" });
+    const heading3 = screen.getByRole("heading", { name: "test3" });
+
+    expect(heading1).toBeInTheDocument();
+    expect(heading2).toBeInTheDocument();
+    expect(heading3).toBeInTheDocument();
   });
 });
--- src/components/flowing-assets/flowing-assets-desktop/flowing-assets-desktop.tsx
+++ src/components/flowing-assets/flowing-assets-desktop/flowing-assets-desktop.tsx
@@ -1,11 +1,11 @@
-import { Assets } from "../flowing-assets-types";
+import { Zones } from "../flowing-assets-types";
 
 type Props = {
-  assets: Assets;
+  zones: Zones;
 };
 
-const FlowingAssets_Desktop: React.FC<Props> = () => {
-  return <></>;
+const FlowingAssets_Desktop: React.FC<Props> = ({ zones }) => {
+  return zones.map((zone) => <h1 key={zone.id}>{zone.name}</h1>);
 };
 
 export default FlowingAssets_Desktop;
