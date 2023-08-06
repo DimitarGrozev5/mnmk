d87b358d234e66ac1c42c9a9653119ec7da37b3c
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2023-08-06 12:37:14 +0300

    remove unused styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import "./App.css";
 import FlowingAssets_Desktop from "./components/flowing-assets/flowing-assets-desktop/flowing-assets-desktop";
 import { mockZones } from "./components/flowing-assets/mock-zones";
 
 function App() {
   return (
     <>
-      <div className="flex flex-col w-full items-stretch">
+      <div className="flex flex-col w-full items-stretch border border-black">
         <FlowingAssets_Desktop zones={mockZones} />
       </div>
     </>
