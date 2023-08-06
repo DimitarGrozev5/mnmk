e5efdffee870aec77c42d08b3dc468c913f0983d
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2023-08-06 14:52:18 +0300

    update Zones type

--- src/components/flowing-assets/flowing-assets-types.ts
+++ src/components/flowing-assets/flowing-assets-types.ts
@@ -1,6 +1,12 @@
+export type Zones = Zone[];
+
 export type Zone = {
   id: string;
   name: string;
+  assets: Asset[];
 };
 
-export type Zones = Zone[];
+export type Asset = {
+  id: string;
+  component: React.ReactNode;
+};
--- src/components/flowing-assets/mock-zones.ts
+++ src/components/flowing-assets/mock-zones.ts
@@ -1,7 +1,31 @@
 import { type Zones } from "./flowing-assets-types";
 
 export const mockZones = (): Zones => [
-  { id: "1", name: "Test1" },
-  { id: "2", name: "Test2" },
-  { id: "3", name: "Test3" },
+  {
+    id: "1",
+    name: "Test1",
+    assets: [
+      { id: "1", component: "TestAsset1" },
+      { id: "2", component: "TestAsset2" },
+      { id: "3", component: "TestAsset3" },
+    ],
+  },
+  {
+    id: "2",
+    name: "Test2",
+    assets: [
+      { id: "1", component: "TestAsset4" },
+      { id: "2", component: "TestAsset5" },
+      { id: "3", component: "TestAsset6" },
+    ],
+  },
+  {
+    id: "3",
+    name: "Test3",
+    assets: [
+      { id: "1", component: "TestAsset7" },
+      { id: "2", component: "TestAsset8" },
+      { id: "3", component: "TestAsset9" },
+    ],
+  },
 ];
