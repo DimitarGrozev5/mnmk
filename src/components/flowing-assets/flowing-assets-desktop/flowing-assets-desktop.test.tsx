a15bc3c3e3346e1bdad5f61fcb025ed4ac4bac1b
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2023-08-06 12:15:22 +0300

    move mockZones to file

--- src/components/flowing-assets/flowing-assets-desktop/flowing-assets-desktop.test.tsx
+++ src/components/flowing-assets/flowing-assets-desktop/flowing-assets-desktop.test.tsx
@@ -1,17 +1,11 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import FlowingAssets_Desktop from "./flowing-assets-desktop";
-import { Zones } from "../flowing-assets-types";
-
-const mockData: Zones = [
-  { id: "1", name: "test1" },
-  { id: "2", name: "test2" },
-  { id: "3", name: "test3" },
-];
+import { mockZones } from "../mock-zones";
 
 describe("FlowingAssets_Desktop", () => {
   it("should render all asset zones with a name", () => {
-    render(<FlowingAssets_Desktop zones={mockData} />);
+    render(<FlowingAssets_Desktop zones={mockZones} />);
 
     const heading1 = screen.getByRole("heading", { name: "test1" });
     const heading2 = screen.getByRole("heading", { name: "test2" });
---
+++ src/components/flowing-assets/mock-zones.ts
@@ -0,0 +1,7 @@
+import { type Zones } from "./flowing-assets-types";
+
+export const mockZones: Zones = [
+  { id: "1", name: "test1" },
+  { id: "2", name: "test2" },
+  { id: "3", name: "test3" },
+];
