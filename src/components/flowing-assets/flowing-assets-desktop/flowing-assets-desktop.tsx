9cd817263af9b7c778904ac3fea8b9835f20d5a3
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2023-08-06 23:05:06 +0300

    sort transformers according to source assets

--- src/components/flowing-assets/flowing-assets-desktop/flowing-assets-desktop.tsx
+++ src/components/flowing-assets/flowing-assets-desktop/flowing-assets-desktop.tsx
@@ -1,5 +1,9 @@
+import React, { useMemo } from "react";
 import clsx from "clsx";
-import { ZonesAndTransformers } from "../flowing-assets-types";
+import type {
+  Transformer,
+  ZonesAndTransformers,
+} from "../flowing-assets-types";
 import FlowingZone from "./flowing-zone";
 import { useRefsArray } from "./hooks/useRefsArray";
 import FlowingTransformer from "./flowing-transformer";
@@ -11,6 +15,7 @@ type Props = {
 const FlowingAssets_Desktop: React.FC<Props> = ({
   zonesAndTransformers: { zones, transformers },
 }) => {
+  // Get refs for zones and transformers
   const {
     refs: assetRefs,
     addRef: addAssetRef,
@@ -23,15 +28,33 @@ const FlowingAssets_Desktop: React.FC<Props> = ({
     removeRef: removeTransRef,
   } = useRefsArray();
 
+  // Sort and filter transformers
+  const transformerZones: Transformer[][] = useMemo(
+    () =>
+      transformers.reduce((acc, transformer) => {
+        // Find the zone that the transformer takes it's sorurces
+        const zoneIndex = zones.findIndex((zone) =>
+          zone.assets.find((asset) => transformer.sources.includes(asset))
+        );
+        acc[zoneIndex].push(transformer);
+        return acc;
+      }, zones.map(() => []) as Transformer[][]),
+    [transformers, zones]
+  );
+
+  console.log(transformerZones);
+
   return (
     <div className={clsx("flex flex-col gap-4", "bg-slate-200", "p-3")}>
       {zones.map((zone) => (
-        <FlowingZone
-          key={zone.id}
-          zone={zone}
-          addAssetRef={addAssetRef}
-          removeAssetRef={removeAssetRef}
-        />
+        <React.Fragment key={zone.id}>
+          <FlowingZone
+            key={zone.id}
+            zone={zone}
+            addAssetRef={addAssetRef}
+            removeAssetRef={removeAssetRef}
+          />
+        </React.Fragment>
       ))}
       <div className={clsx("flex flex-row gap-4", "px-5")}>
         {transformers.map((transformer) => (
