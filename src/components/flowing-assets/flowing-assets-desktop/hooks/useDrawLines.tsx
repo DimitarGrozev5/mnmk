d24cd537fab596ef1a17b6e472b1696016d23882
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2023-08-08 22:34:24 +0300

    add easing function

--- src/components/flowing-assets/flowing-assets-desktop/hooks/useDrawLines.tsx
+++ src/components/flowing-assets/flowing-assets-desktop/hooks/useDrawLines.tsx
@@ -55,15 +55,61 @@ export const useDrawLines = (
       );
     }
 
+    const start = new Date().getTime();
+    const animationDuration = 1000;
+
+    function easeInOutCubic(x: number): number {
+      return x === 1 ? 1 : 1 - Math.pow(2, -10 * x);
+    }
+
     const draw = () => {
-      lines.forEach((line) => {
+      const dt = Math.min(new Date().getTime() - start, animationDuration);
+
+      const scale = easeInOutCubic(dt / animationDuration);
+
+      const lineLengths = lines.map((line) => {
+        let len = 0;
+        for (let index = 1; index < line.length; index++) {
+          len += Math.abs(line[index][0] - line[index - 1][0]);
+          len += Math.abs(line[index][1] - line[index - 1][1]);
+        }
+        return len;
+      });
+
+      lines.forEach((line, index) => {
         ctx.beginPath();
         ctx.moveTo(line[0][0], line[0][1]);
-        line.slice(1).forEach((segment) => {
-          ctx.lineTo(segment[0], segment[1]);
+
+        let accLen = 0;
+        const targetLen = lineLengths[index] * scale;
+
+        line.slice(1).forEach((pt, ptIndex) => {
+          if (accLen > targetLen) {
+            return;
+          }
+          const prevPt = line[ptIndex];
+
+          const currLen =
+            Math.abs(pt[0] - prevPt[0]) + Math.abs(pt[1] - prevPt[1]);
+          if (accLen + currLen > targetLen) {
+            const ptDir = [
+              Math.sign(pt[0] - prevPt[0]),
+              Math.sign(pt[1] - prevPt[1]),
+            ];
+
+            const ptX = prevPt[0] + (targetLen - accLen) * ptDir[0];
+            const ptY = prevPt[1] + (targetLen - accLen) * ptDir[1];
+            ctx.lineTo(ptX, ptY);
+
+            accLen += currLen;
+            return;
+          }
+          accLen += currLen;
+          ctx.lineTo(pt[0], pt[1]);
         });
         ctx.stroke();
       });
+
       if (keepDrawing) {
         window.requestAnimationFrame(draw);
       } else {
