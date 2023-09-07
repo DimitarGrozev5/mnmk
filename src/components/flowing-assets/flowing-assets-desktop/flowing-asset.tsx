5156ae9d2802b99498c5fea282692e0f9fc0c6d4
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2023-09-07 19:30:41 +0300

    show add element depending on type

--- src/components/flowing-assets/flowing-assets-desktop/flowing-asset.tsx
+++ src/components/flowing-assets/flowing-assets-desktop/flowing-asset.tsx
@@ -1,5 +1,6 @@
 import { useAppSelector } from "../../../store/hooks";
 import type { ElementId } from "../../../store/slices/flowing-assets-types";
+import FlowingAddNewAsset from "./flowing-add-new-asset";
 import FlowingElement from "./flowing-element/flowing-element";
 
 type Props = {
@@ -10,6 +11,10 @@ const FlowingAsset: React.FC<Props> = ({ assetId }) => {
   const asset = useAppSelector(
     (state) => state.zonesAndTransformers.assets[assetId]
   );
+
+  if (asset.type === "add_new") {
+    return <FlowingAddNewAsset id={assetId} />;
+  }
 
   return (
     <FlowingElement id={assetId} type="assets" rectangular>
