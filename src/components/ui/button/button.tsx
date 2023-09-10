736cb225891f147522f9979094cd5699870d4377
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2023-09-10 14:58:48 +0300

    add button display variants

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,20 +1,34 @@
 import clsx from "clsx";
 
 type Props = {
-  onClick: () => void;
+  onClick?: () => void;
+  variant?: "plain" | "contained";
+  uppercase?: boolean;
   children: React.ReactNode;
 };
 
-const Button: React.FC<Props> = ({ onClick, children }) => {
+const Button: React.FC<Props> = ({
+  onClick,
+  variant = "plain",
+  uppercase = true,
+  children,
+}) => {
   return (
     <button
       onClick={onClick}
       className={clsx(
+        "inline-flex flex-col items-stretch",
         "p-1",
         "transition-all duration-100",
-        "text-slate-600 uppercase",
+        "text-slate-600",
+        uppercase && "uppercase",
         "hover:text-slate-400",
-        "active:scale-95"
+        "active:scale-95",
+        variant === "contained" && "bg-slate-200",
+        variant === "contained" && "border border-slate-400 rounded-lg",
+        variant === "contained" && "py-2 px-4",
+        variant === "contained" &&
+          "hover:bg-slate-300 hover:border-slate-500 hover:text-slate-700"
       )}
     >
       {children}
