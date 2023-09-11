2408ab5fe2fb20a5578005b7dc8dee67a4f64d33
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2023-09-11 10:46:24 +0300

    forward ref to Button component

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import clsx from "clsx";
 
 type Props = {
@@ -7,33 +8,31 @@ type Props = {
   children: React.ReactNode;
 };
 
-const Button: React.FC<Props> = ({
-  onClick,
-  variant = "plain",
-  uppercase = true,
-  children,
-}) => {
-  return (
-    <button
-      onClick={onClick}
-      className={clsx(
-        "inline-flex flex-col items-stretch",
-        "p-1",
-        "transition-all duration-100",
-        "text-slate-600",
-        uppercase && "uppercase",
-        "hover:text-slate-400",
-        "active:scale-95",
-        variant === "contained" && "bg-slate-200",
-        variant === "contained" && "border border-slate-400 rounded-lg",
-        variant === "contained" && "py-2 px-4",
-        variant === "contained" &&
-          "hover:bg-slate-300 hover:border-slate-500 hover:text-slate-700"
-      )}
-    >
-      {children}
-    </button>
-  );
-};
+const Button = React.forwardRef<HTMLButtonElement, Props>(
+  ({ onClick, variant = "plain", uppercase = true, children }, ref) => {
+    return (
+      <button
+        ref={ref}
+        onClick={onClick}
+        className={clsx(
+          "inline-flex flex-col items-stretch",
+          "p-1",
+          "transition-all duration-100",
+          "text-slate-600",
+          uppercase && "uppercase",
+          "hover:text-slate-400",
+          "active:scale-95",
+          variant === "contained" && "bg-slate-200",
+          variant === "contained" && "border border-slate-400 rounded-lg",
+          variant === "contained" && "py-2 px-4",
+          variant === "contained" &&
+            "hover:bg-slate-300 hover:border-slate-500 hover:text-slate-700"
+        )}
+      >
+        {children}
+      </button>
+    );
+  }
+);
 
 export default Button;
