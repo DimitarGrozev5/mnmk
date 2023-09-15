2fe95c2f77c949ae073a3937c59d30e2486cdeb5
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2023-09-15 17:16:43 +0300

    styled switch

--- src/components/ui/input-label/input-label.tsx
+++ src/components/ui/input-label/input-label.tsx
@@ -3,14 +3,15 @@ import { tw } from "../../../util/tw";
 type Props = {
   htmlFor: string;
   children: string;
+  className?: string;
 };
 
-const InputLabel: React.FC<Props> = ({ htmlFor, children }) => {
+const InputLabel: React.FC<Props> = ({ htmlFor, children, className }) => {
   return (
     <label
       onClick={(e) => e.preventDefault()}
       htmlFor={htmlFor}
-      className={tw("cursor-pointer")}
+      className={tw("cursor-pointer", className)}
     >
       {children}
     </label>
