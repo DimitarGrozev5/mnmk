bd7c59dc9ae3969cd07b8889c66fb8174fb225b8
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2023-09-20 22:35:09 +0300

    implement headless ui modal

--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -1,6 +1,7 @@
-import Overlay from "./overlay";
 import Button from "../button/button";
 import { tw } from "../../../util/tw";
+import { Dialog, Transition } from "@headlessui/react";
+import React from "react";
 
 type Props = {
   show: boolean;
@@ -20,41 +21,76 @@ const Modal: React.FC<Props> = ({
   fullScreen = false,
 }) => {
   return (
-    <>
-      <Overlay show={show} onClose={onClose} blur />
-      <div
-        className={tw(
-          show ? "visible opacity-100" : "invisible opacity-5",
-          "fixed z-50",
-          "p-4 rounded-lg",
-          "flex flex-col items-stretch gap-4",
-          "bg-slate-300",
-          "transition-all duration-500"
-        )}
-        style={{
-          left: fullScreen ? "5vw" : "50vw",
-          top: show ? (fullScreen ? "5vh" : "10vh") : "-50vh",
-          width: fullScreen ? "90vw" : "40vw",
-          height: fullScreen ? "90vh" : "60vh",
-          transform: fullScreen ? "translate(0%, 0%)" : "translate(-50%, 0%)",
-        }}
-      >
-        {title && (
-          <h1 className={tw("text-2xl font-bold text-slate-500 text-center")}>
-            {title}
-          </h1>
-        )}
+    <Transition appear show={show} as={React.Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={onClose}>
+        <Transition.Child
+          as={React.Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div
+            onClick={onClose}
+            className={tw("fixed inset-0", "bg-black bg-opacity-40")}
+          />
+        </Transition.Child>
 
-        <div className={tw("flex-1", "flex flex-col items-center gap-4")}>
-          {children}
-        </div>
+        <div
+          className={tw(
+            "fixed z-50",
+            "p-4 rounded-lg",
+            "flex flex-col items-stretch gap-4",
+            "bg-slate-300"
+          )}
+          style={{
+            left: fullScreen ? "5vw" : "50vw",
+            top: fullScreen ? "5vh" : "10vh",
+            width: fullScreen ? "90vw" : "40vw",
+            height: fullScreen ? "90vh" : "60vh",
+            transform: fullScreen ? "translate(0%, 0%)" : "translate(-50%, 0%)",
+          }}
+        >
+          <Transition.Child
+            as={React.Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Dialog.Panel
+              className={tw("flex-1", "flex flex-col items-stretch gap-4")}
+            >
+              {title && (
+                <Dialog.Title
+                  as="h3"
+                  className={tw(
+                    "text-2xl font-bold text-slate-500 text-center"
+                  )}
+                >
+                  {title}
+                </Dialog.Title>
+              )}
 
-        <div className={tw("flex flex-row gap-4 justify-end items-center")}>
-          <Button onClick={onClose}>Close</Button>
-          {actions}
+              <div className={tw("flex-1", "flex flex-col items-center gap-4")}>
+                {children}
+              </div>
+
+              <div
+                className={tw("flex flex-row gap-4 justify-end items-center")}
+              >
+                <Button onClick={onClose}>Close</Button>
+                {actions}
+              </div>
+            </Dialog.Panel>
+          </Transition.Child>
         </div>
-      </div>
-    </>
+      </Dialog>
+    </Transition>
   );
 };
 
