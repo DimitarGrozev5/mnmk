d3839b94f65ab08c1fa269edc36481488dc5b71e
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2023-09-21 12:41:00 +0300

    add validation functions

--- src/components/common/file-parser/column-types.ts
+++ src/components/common/file-parser/column-types.ts
@@ -1,17 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const isNumber = (value: string) => !Number.isNaN(Number(value));
+
 export const fileColumns = {
-  unset: { label: "Null" },
-  number: { label: "Number" },
-  pointName: { label: "Name" },
-  northing: { label: "N" },
-  easting: { label: "E" },
-  height: { label: "H" },
-  instrumentHeight: { label: "Vi" },
-  observedName: { label: "Observed Name" },
-  observedHeight: { label: "Vs" },
-  horizontalAngle: { label: "R" },
-  verticalAngle: { label: "Z" },
-  slantDistance: { label: "S" },
-  horizontalDistance: { label: "D" },
+  unset: { label: "Null", validate: (_: string) => true },
+  number: { label: "Number", validate: isNumber },
+  pointName: { label: "Name", validate: (value: string) => value.length > 0 },
+  northing: { label: "N", validate: isNumber },
+  easting: { label: "E", validate: isNumber },
+  height: { label: "H", validate: isNumber },
+  instrumentHeight: { label: "Vi", validate: isNumber },
+  observedName: {
+    label: "Observed Name",
+    validate: (value: string) => value.length > 0,
+  },
+  observedHeight: { label: "Vs", validate: isNumber },
+  horizontalAngle: { label: "R", validate: isNumber },
+  verticalAngle: { label: "Z", validate: isNumber },
+  slantDistance: { label: "S", validate: isNumber },
+  horizontalDistance: { label: "D", validate: isNumber },
 };
 
 export type FileColumn = keyof typeof fileColumns;
