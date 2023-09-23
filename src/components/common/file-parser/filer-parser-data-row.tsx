e4b434bcf20e4e8eb54b97aab96b7fec4c0b407c
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2023-09-23 21:21:16 +0300

    display modal for editing fields

--- src/components/common/file-parser/filer-parser-data-row.tsx
+++ src/components/common/file-parser/filer-parser-data-row.tsx
@@ -1,10 +1,14 @@
-import { useMemo } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { tw } from "../../../util/tw";
 import { FileColumn, fileColumns } from "./column-types";
 import { PencilSquareIcon, XMarkIcon } from "@heroicons/react/20/solid";
 import IconButton from "../../ui/button/icon-button";
-import { useAppSelector } from "../../../store/hooks";
-import { getLineWithFields } from "../../../store/slices/file-parser-slice";
+import { useAppDispatch, useAppSelector } from "../../../store/hooks";
+import {
+  fileParserActions,
+  getLineWithFields,
+} from "../../../store/slices/file-parser-slice";
+import Modal from "../../ui/modal/modal";
 
 type Props = {
   rowId: string;
@@ -13,72 +17,110 @@ type Props = {
 };
 
 const FileParserDataRow: React.FC<Props> = ({ rowId, index, fields }) => {
-  const line = useAppSelector(getLineWithFields(rowId));
+  const line = useAppSelector((state) => getLineWithFields(state, rowId));
+  const dispatch = useAppDispatch();
+  const { removeLine } = fileParserActions;
+
+  const [selectedFieldId, setSelectedFieldId] = useState<string | null>(null);
+  const [editValue, setEditValue] = useState("");
+  useEffect(() => {
+    selectedFieldId !== null &&
+      setEditValue(
+        line.find((field) => field.id === selectedFieldId)?.value ?? ""
+      );
+  }, [line, selectedFieldId]);
 
   const fieldsValidity = useMemo(
     () =>
       line.map((field) => {
         const fieldType = fileColumns[fields[index] ?? "unset"];
-        return fieldType.validate(field);
+        return fieldType.validate(field.value);
       }),
     [fields, index, line]
   );
 
   const rowIsValid = fieldsValidity.every((isValid) => isValid);
 
+  const onRemoveLine = useCallback(() => {
+    dispatch(removeLine(rowId));
+  }, [dispatch, removeLine, rowId]);
+
   return (
-    <tr className={tw("group/row", "w-[max-content]", "odd:bg-slate-3001")}>
-      <th
-        className={tw(
-          "border border-slate-400 px-2",
-          "text-sm text-slate-400 font-normal",
-          "group-odd/row:bg-slate-300",
-          !rowIsValid && "text-red-500 border-red-500 bg-red-50"
-        )}
-      >
-        {index + 1}
-      </th>
-      {line.map((field, indexField) => (
-        <td
-          key={`${index}-${indexField}`}
+    <>
+      <tr className={tw("group/row", "w-[max-content]", "odd:bg-slate-3001")}>
+        <th
           className={tw(
-            "relative",
-            "group/field",
-            "border border-slate-300 px-3",
-            "text-slate-800",
+            "border border-slate-400 px-2",
+            "text-sm text-slate-400 font-normal",
             "group-odd/row:bg-slate-300",
-            !fieldsValidity[indexField] && "text-red-500"
+            !rowIsValid && "text-red-500 border-red-500 bg-red-50"
           )}
         >
-          {field}
-          <div
+          {index + 1}
+        </th>
+        {line.map((field, indexField) => (
+          <td
+            key={field.id}
             className={tw(
-              "absolute right-1 inset-y-0",
-              "flex flex-col justify-center",
-              "transition-all duration-500",
-              "opacity-0 invisible",
-              "group-hover/field:opacity-100 group-hover/field:visible"
+              "relative",
+              "group/field",
+              "border border-slate-300 px-3",
+              "text-slate-800",
+              "group-odd/row:bg-slate-300",
+              !fieldsValidity[indexField] && "text-red-500"
             )}
           >
-            <IconButton label="Delete line">
-              <PencilSquareIcon className="w-4 h-4 text-sky-500" />
-            </IconButton>
-          </div>
+            {field.value}
+            <div
+              className={tw(
+                "absolute right-1 inset-y-0",
+                "flex flex-col justify-center",
+                "transition-all duration-500",
+                "opacity-0 invisible",
+                "group-hover/field:opacity-100 group-hover/field:visible"
+              )}
+            >
+              <IconButton
+                label="Delete line"
+                onClick={() => setSelectedFieldId(field.id)}
+              >
+                <PencilSquareIcon className="w-4 h-4 text-sky-500" />
+              </IconButton>
+            </div>
+          </td>
+        ))}
+        <td
+          className={tw(
+            "flex flex-col items-center justify-center",
+            "transition-all duration-500",
+            "opacity-0 invisible",
+            "group-hover/row:opacity-100 group-hover/row:visible"
+          )}
+        >
+          <IconButton onClick={onRemoveLine} label="Delete line">
+            <XMarkIcon className="w-6 h-6 text-sky-500" />
+          </IconButton>
         </td>
-      ))}
-      <td
-        className={tw(
-          "flex flex-col items-center justify-center",
-          "transition-all duration-500",
-          "opacity-0 invisible",
-          "group-hover/row:opacity-100 group-hover/row:visible"
-        )}
+      </tr>
+
+      <Modal
+        show={selectedFieldId !== null}
+        onClose={() => setSelectedFieldId(null)}
       >
-        <IconButton label="Delete line">
-          <XMarkIcon className="w-6 h-6 text-sky-500" />
-        </IconButton>
-      </td>
-    </tr>
+        <div
+          className={tw(
+            "self-stretch",
+            "flex flex-row justify-between items-center gap-2",
+            "border border-red-500"
+          )}
+        >
+          <input
+            value={editValue}
+            onChange={(e) => setEditValue(e.target.value)}
+          />
+        </div>
+      </Modal>
+    </>
   );
 };
 
--- src/store/slices/file-parser-slice.ts
+++ src/store/slices/file-parser-slice.ts
@@ -1,4 +1,4 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSelector, createSlice } from "@reduxjs/toolkit";
 import { nanoid } from "nanoid";
 import { RootState } from "../store";
 
@@ -41,6 +41,37 @@ export const fileParserSlice = createSlice({
       state.fields = fields;
       state.lines = lines;
     },
+    removeLine: (state, action: PayloadAction<string>) => {
+      const lineId = action.payload;
+      state.linesArray = state.linesArray.filter((line) => {
+        if (line === lineId) {
+          state.lines[lineId].forEach((fieldId) => {
+            delete state.fields[fieldId];
+          });
+          return false;
+        }
+        return true;
+      });
+      delete state.lines[lineId];
+    },
+    editField: (
+      state,
+      action: PayloadAction<{ id: string; value: string }>
+    ) => {
+      const { id, value } = action.payload;
+      state.fields[id] = value;
+
+      if (value.length === 0) {
+        delete state.fields[id];
+
+        Object.entries({ ...state.lines }).forEach(([lineId, line]) => {
+          state.lines[lineId] = line.filter((fieldId) => fieldId !== id);
+          if (state.lines[lineId].length === 0) {
+            delete state.lines[lineId];
+          }
+        });
+      }
+    },
   },
 });
 
@@ -54,8 +85,24 @@ export const getAllLinesIds = () => (state: RootState) =>
 export const getLineById = (id: string) => (state: RootState) =>
   state.fileParser.lines[id];
 
-export const getLineWithFields = (id: string) => (state: RootState) =>
-  state.fileParser.lines[id].map((fieldId) => state.fileParser.fields[fieldId]);
+export const getLineWithFields1 = (id: string) => (state: RootState) =>
+  state.fileParser.lines[id].map((fieldId) => ({
+    id: fieldId,
+    value: state.fileParser.fields[fieldId],
+  }));
+
+export const getLineWithFields = createSelector(
+  [
+    (state: RootState) => state.fileParser.lines,
+    (state: RootState) => state.fileParser.fields,
+    (_: RootState, id: string) => id,
+  ],
+  (lines, fields, id: string) =>
+    lines[id].map((fieldId) => ({
+      id: fieldId,
+      value: fields[fieldId],
+    }))
+);
 
 // Action creators are generated for each case reducer function
 export const fileParserActions = {
